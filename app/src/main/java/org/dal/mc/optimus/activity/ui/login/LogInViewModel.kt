53386895e250cba9aef98fc26b4e5548249a864a
Satya Kumar Itekela <st798799@dal.ca>
2020-03-03 23:53:17 -0400

    adding login module

---
+++ app/src/main/java/org/dal/mc/optimus/activity/ui/login/LogInViewModel.kt
@@ -0,0 +1,7 @@
+package org.dal.mc.optimus.activity.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LogInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/org/dal/mc/optimus/activity/ui/login/SignUpFragment.kt
@@ -0,0 +1,33 @@
+package org.dal.mc.optimus.activity.ui.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import org.dal.mc.optimus.R
+
+class SignUpFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignUpFragment()
+    }
+
+    private lateinit var viewModel: SignUpViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SignUpViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
